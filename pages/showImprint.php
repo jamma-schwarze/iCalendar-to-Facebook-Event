843b741cd64c3ea84e45148abdc11f60b5e2ef4d
Jamma Tino Schwarze <github@tisc.de>
2013-11-12 19:48:27 +0100

    Update docs, add liks to privacy policy and imprint

---
+++ pages/showImprint.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'headerInclude.php';
+?>
+
+<h2>Imprint</h2>
+
+<p>This application is provided without any warranty.</p>
+
+<p>For support, please visit the <a href="https://www.facebook.com/pages/Calendar-to-Events-Importer-Community/536493229767772">Calendar to Events Importer Community</a> on Facebook.</p>
+
+<p>Jamma Tino Schwarze</p>
+
+<?php
+include 'footerInclude.php';
+?>
