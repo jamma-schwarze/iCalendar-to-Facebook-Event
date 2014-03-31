7b506b04e10715eeaad8e5c1b4bc18828850b897
Jamma Tino Schwarze <github@tisc.de>
2014-03-31 21:16:10 +0200

    also run subscription through module to check FB times

--- tests/importtest.php
+++ tests/importtest.php
@@ -6,12 +6,14 @@ require_once 'include/initialize.php';
 
 $importer = new iCalcreatorImporter();
 
-$calUrl = '/tmp/lz-1.ics';
-$calUrl = 'tests/lz-2.ics';
+#$calUrl = '/tmp/lz-1.ics';
+#$calUrl = 'tests/lz-2.ics';
 
 #$calUrl = 'file:///Users/maurobieg/Sites/calendar_to_facebook_tschurrn/tests/files/g.ics';
 
 #$calUrl = 'http://ctip.org.uk/index.php?option=com_eventlist&view=eventlist&format=ical&Itemid=2';
+
+$calUrl = 'https://ical2fb.lichtzentrum-meissen.de/tests/xxx.ics';
 
 $subId = null;
 
@@ -26,6 +28,21 @@ $importer->downloadAndParse(
 	$eventXProperties
 );
 
+if (php_sapi_name() != "cli")
+{
+	echo '<html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8"></head>';
+	echo "<body><pre>";
+}
+
 var_dump($importer->subscription);
 
+$moduleNames = array();
+$moduleNames[] = 'standardModule';
+
+echo "apply modules...\n";
+
+//TODO: select right modules
+$importer->applyModules($moduleNames);
+
+var_dump($importer->subscription);
 ?>
